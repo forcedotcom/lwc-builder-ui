c22ea4e0df6afeb41287726db702ebdbd3238f83
Alba Rivas <arivas@salesforce.com>
2021-02-08 11:47:02 +0100

    Add dummy test

--- src/modules/my/form/__tests__/form.test.js
+++ src/modules/my/form/__tests__/form.test.js
@@ -5,11 +5,23 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { createElement } from 'lwc';
+import MyForm from 'my/form';
+
 describe('my-form', () => {
   afterEach(() => {
     // The jsdom instance is shared across test cases in a single file so reset the DOM
     while (document.body.firstChild) {
       document.body.removeChild(document.body.firstChild);
     }
+  });
+
+  it('dummy test', () => {
+    const element = createElement('my-form', {
+      is: MyForm
+    });
+    document.body.appendChild(element);
+
+    expect(1).toBe(1);
   });
 });
