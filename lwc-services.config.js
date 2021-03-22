cb2239374897822c3950708b62d45578a0e90d52
ninoish <ninoish.dev@gmail.com>
2021-03-23 00:48:26 +0900

    remove slds

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -3,11 +3,5 @@
 module.exports = {
   buildDir: './dist',
   sourceDir: './src',
-  resources: [
-    { from: 'src/resources/', to: 'dist/resources/' },
-    {
-      from: 'node_modules/@salesforce-ux/design-system/assets/',
-      to: 'dist/resources/slds/'
-    }
-  ]
+  resources: [{ from: 'src/resources/', to: 'dist/resources/' }]
 };
